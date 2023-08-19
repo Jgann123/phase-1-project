b1515f351761f3108916f3d61fa2bac5abe7daf5
[Jeremy Gann] <[jeremygann@beitzelcs.com]>
2023-08-18 22:05:49 -0400

    added CDN for bootstrap a button and a onclick function

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-$.ajax({
-    type:"GET",
-    url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Ilt0n51VEcZ5oT8ImFifRM19ZAtUjEpY",
-    async:true,
-    dataType: "json",
-    success: function(json) {
-                console.log(json);
-                // Parse the response.
-                // Do other things.
-             },
-    error: function(xhr, status, err) {
-                // This time, we do not end up here!
-             }
-  });
+function ajaxCall() {
+   console.log("ajax call is firing!")
+   $.ajax({
+      type:"GET",
+      url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Ilt0n51VEcZ5oT8ImFifRM19ZAtUjEpY",
+      async:true,
+      dataType: "json",
+      success: function(json) {
+                  console.log(json);
+                  // Parse the response.
+                  // Do other things.
+               },
+      error: function(xhr, status, err) {
+                  // This time, we do not end up here!
+               }
+    });
+}
